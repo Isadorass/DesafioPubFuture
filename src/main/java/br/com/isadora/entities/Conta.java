209ff0d012c25aafa38ef73cc9c4aae71f83c9a9
Isadora <isadorass1710@gmail.com>
2022-01-10 21:53:59 -0300

    Adicionando documentação a algumas entidades

--- src/main/java/br/com/isadora/entities/Conta.java
+++ src/main/java/br/com/isadora/entities/Conta.java
@@ -10,21 +10,131 @@ import org.springframework.lang.NonNull;
 
 import br.com.isadora.enums.TipoConta;
 
+/**
+ * <h1>Entidade referente a {@link Conta}.</h1>
+ * 
+ * <p>Está classe possui todos os
+ * atributos da entidade {@link Conta}.
+ * Está classe foi mapeada com o
+ * framework Hibernate para que seja
+ * feita uma entidade relacional em
+ * um banco de dados.</p>
+ * 
+ * <p>Esta classe recebe a anotação
+ * {@link Entity}, que diz ao Hibernate
+ * que esta classe será uma entidade
+ * em nosso banco de dados.</p>
+ * 
+ * @author Isadora de Souza e Silva <strong>isadorass1710@gmail.com</strong>
+ *
+ * @see Conta
+ * @see Entity
+ */
 @Entity
 public class Conta {
 
+	/**
+	 * <p>id {@link Integer} - Referente ao Id
+	 * da {@link Conta}.</p>
+	 * 
+	 * <p>Este atributo recebe
+	 * a anotação {@link GeneratedValue} com 
+	 * o seu parâmetro strategy. Está anotação
+	 * determina que esse atributo vai ser gerado
+	 * automaticamente com a estratégia {@link GenerationType#AUTO}.</p>
+	 * 
+	 * <p>Este atributo também recebe
+	 * a anotação {@link Id}, essa anotação
+	 * diz para o Hibernate que este atributo
+	 * será a chave primária da entidade
+	 * {@link Conta}.</p>
+	 * 
+	 * @see Integer
+	 * @see Conta
+	 * @see GeneratedValue
+	 * @see GenerationType#AUTO
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
+	/**
+	 * <p>saldo {@link Double} - Referente
+	 * ao saldo da {@link Conta}.</p>
+	 * 
+	 * <p>Este atributo recebe a anotação
+	 * {@link NonNull}, que diz ao Hibernate
+	 * que este atributo não poderá ser
+	 * salvo como nulo no banco de dados.</p>
+	 * 
+	 * <p>Este atributo recebe a anotação
+	 * {@link Column}, a anotação {@link Column}
+	 * diz ao Hibernate que este atributo
+	 * será uma coluna da entidade {@link Conta}.
+	 * Usamos um parametro {@link Column#length()}
+	 * para determinar o tamanho deste atributo
+	 * no banco de dados.</p>
+	 * 
+	 * @see Double
+	 * @see Conta
+	 * @see NonNull
+	 * @see Column
+	 * @see Column#length()
+	 */
 	@NonNull
 	@Column(length = 99999999)
 	private Double saldo;
 
+	/**
+	 * <p>tipoConta {@link TipoConta} - Referente
+	 * ao tipo da {@link Conta}.</p>
+	 * 
+	 * <p>Este atributo recebe a anotação
+	 * {@link NonNull}, que diz ao Hibernate
+	 * que este atributo não poderá ser
+	 * salvo como nulo no banco de dados.</p>
+	 * 
+	 * <p>Este atributo recebe a anotação
+	 * {@link Column}, a anotação {@link Column}
+	 * diz ao Hibernate que este atributo
+	 * será uma coluna da entidade {@link Conta}.
+	 * Usamos um parametro {@link Column#length()}
+	 * para determinar o tamanho deste atributo
+	 * no banco de dados.</p>
+	 * 
+	 * @see TipoConta
+	 * @see Conta
+	 * @see NonNull
+	 * @see Column
+	 * @see Column#length()
+	 */
 	@NonNull
 	@Column(length = 14)
 	private TipoConta tipoConta;
 
+	/**
+	 * <p>instituicaoFinanceira {@link String} - Referente
+	 * a instituição finaceira da {@link Conta}.</p>
+	 * 
+	 * <p>Este atributo recebe a anotação
+	 * {@link NonNull}, que diz ao Hibernate
+	 * que este atributo não poderá ser
+	 * salvo como nulo no banco de dados.</p>
+	 * 
+	 * <p>Este atributo recebe a anotação
+	 * {@link Column}, a anotação {@link Column}
+	 * diz ao Hibernate que este atributo
+	 * será uma coluna da entidade {@link Conta}.
+	 * Usamos um parametro {@link Column#length()}
+	 * para determinar o tamanho deste atributo
+	 * no banco de dados.</p>
+	 * 
+	 * @see String
+	 * @see Conta
+	 * @see NonNull
+	 * @see Column
+	 * @see Column#length()
+	 */
 	@NonNull
 	@Column(length = 255)
 	private String instituicaoFinanceira;
--- src/main/java/br/com/isadora/enums/TipoDespesa.java
+++ src/main/java/br/com/isadora/enums/TipoDespesa.java
@@ -1,5 +1,56 @@
 package br.com.isadora.enums;
 
+import br.com.isadora.entities.Despesa;
+
+/**
+ * <h1>Enum com todos os tipos de {@link Despesa}.</h1>
+ * 
+ * <p>Enum com todos os
+ * valores possíveis para
+ * o tipo de uma {@link Despesa}.</p>
+ * 
+ * <table border = 1>
+ *  <tr>
+ *   <td>Tipo da Conta</td>  
+ *  </tr>
+ *  
+ *  <tr>
+ *   <td>ALIMENTACAO</td>
+ *  </tr>
+ *  
+ *  <tr>
+ *   <td>EDUCACAO</td>
+ *  </tr>
+ *  
+ *  <tr>
+ *   <td>LAZER</td>
+ *  </tr>
+ *  	 
+ *  <tr>
+ *   <td>MORADIA</td>
+ *  </tr>	
+ *   
+ *  <tr>
+ *   <td>ROUPA</td>
+ *  </tr>	
+ *   
+ *  <tr>
+ *   <td>SAUDE</td>
+ *  </tr>
+ *  	 
+ *  <tr>
+ *   <td>TRANSPORTE</td>
+ *  </tr>	
+ *   
+ *  <tr>
+ *   <td>OUTROS</td>
+ *  </tr>	 
+ * </table>
+ * 
+ * @author Isadora de Souza e Silva <strong>isadorass1710@gmail.com</strong>
+ * 
+ * @see Despesa
+ */
 public enum TipoDespesa {
 	ALIMENTACAO,
 	EDUCACAO,
