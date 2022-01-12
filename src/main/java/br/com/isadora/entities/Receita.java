32b54edf05cff91aadcce6906bd0fdbabc1f451a
Isadora <isadorass1710@gmail.com>
2022-01-11 22:01:36 -0300

    Adicionado documentação nas classes services

--- src/main/java/br/com/isadora/entities/Receita.java
+++ src/main/java/br/com/isadora/entities/Receita.java
@@ -14,30 +14,79 @@ import org.springframework.lang.NonNull;
 
 import br.com.isadora.enums.TipoReceita;
 
+/**
+ * Classe referente a entidade Receita
+ * 
+ * @author Isadora de Souza e Silva 
+ * <strong>isadorass1710@gmail.com</strong>
+ *
+ */
 @Entity
 public class Receita {
 
+	/**
+	 * id da receita, chave primária,
+	 * auto incremento.
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
+	/**
+	 * valor, referente ao valor da Receita.
+	 * Tamanho máximo desse campo será 999999999
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@NonNull
 	@Column(length = 999999999)
 	private Double valor;
 
+	/**
+	 * dataRecebimento, referente a data 
+	 * de recebimento da Receita
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@NonNull
 	private LocalDateTime dataRecebimento;
 
+	/**
+	 * dataRecebimentoEsperado, referente a data de 
+	 * recebimento esperado da Receita 
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@NonNull
 	private LocalDateTime dataRecebimentoEsperado;
 
+	/**
+	 * Atributo de descrição referente a Receita
+	 * Tamanho máximo dessa campo será de 255
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@Column(length = 255)
 	private String descricao;
 
+	/**
+	 * tipoReceita referente a Receita
+	 * Tamanho máximo desse campo será de 8
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@NonNull
 	@Column(length = 8)
 	private TipoReceita tipoReceita;
 
+	/**
+	 * Conta, referente a Conta vinculada 
+	 * a esta Receita
+	 * @author Isadora de Souza e Silva 
+	 * <strong>isadorass1710@gmail.com</strong>
+	 */
 	@NonNull
 	@ManyToOne
 	@JoinColumn(name = "conta_id")
--- src/main/java/br/com/isadora/enums/TipoReceita.java
+++ src/main/java/br/com/isadora/enums/TipoReceita.java
@@ -1,5 +1,16 @@
 package br.com.isadora.enums;
 
+/**
+ * Enum com todos os tipos de Receita
+ * 
+ * Enum com todos os
+ * valores possíveis para
+ * o tipo de uma Receita
+ * 
+ * @author Isadora de Souza e Silva 
+ * <strong>isadorass1710@gmail.com</strong>
+ *
+ */
 public enum TipoReceita {
 	SALARIO,
 	PRESENTE,
