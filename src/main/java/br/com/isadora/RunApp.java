e2c1a4e9599ac187420ee67dead4b97934eac15c
Isadora <isadorass1710@gmail.com>
2022-01-12 22:01:27 -0300

    Adicionando métodos de tranferência entre contas e criando classe de exception

--- src/main/java/br/com/isadora/RunApp.java
+++ src/main/java/br/com/isadora/RunApp.java
@@ -3,9 +3,17 @@ package br.com.isadora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <h1>Classe responsável por rodar a aplicação.</h1>
+ * 
+ * <p>Classe responsável por
+ * conter o método principal do projeto
+ * que inicia a aplicação SpringBoot.</p>
+ * 
+ * @author Isadora de Souza e Silva <strong>isadorass1710@gmail.com</strong>
+ */
 @SpringBootApplication
 public class RunApp {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RunApp.class, args);
 	}
